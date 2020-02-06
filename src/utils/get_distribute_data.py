a77e4786bcb116362e547a859ec6d9af2f21cc79
Thorsten Kurth <tkurth@nvidia.com>
2020-02-06 09:22:40 -0800

    debugging

--- src/utils/get_distribute_data.py
+++ src/utils/get_distribute_data.py
@@ -1,6 +1,9 @@
 import os
 import cdsapi
 
+#some raid mod stuff
+nraid = 4
+
 #open client
 c = cdsapi.Client()
 
@@ -18,32 +21,42 @@ years = ['1979', '1980', '1981', '1982']
 #loop over years
 for idx, year in enumerate(years):
     for idy, month in enumerate(months):
-        
-        #distribute round robin in years
-        path = '/data{}/ecmwf_data/gpu{}'.format(idx, (idy // 3) + idx * 4)
-        
-        #filename
-        filename = 'download_'+year+'-'+month+'.nc'
 
-        #outputfile
-        outputfile = os.path.join(path,filename)
-
+        #distribute round robin in years 
+        path = '/data{}/ecmwf_data/gpu{}'.format(2 * (idx % nraid) + 1, (idy // 3) + idx * 4)
+        
         #create folder
         if not os.path.isdir(path):
             os.makedirs(path)
-        
-        #get data
-        c.retrieve(
-            dataset_name,
-            {
-                'product_type': 'reanalysis',
-                'variable': variables,
-                'year': [year],
-                'month': [month],
-                'day': days,
-                'time': times,
-                'format': 'netcdf'
-            },
-            outputfile)
-        break
 
+        #loop over days
+        for idz, day in enumerate(days):
+        
+            #filename
+            filename = dataset_name+'_'+year+'-'+month+'-'+day+'.nc'
+            
+            #outputfile
+            outputfile = os.path.join(path,filename)
+
+            #skip if file exists
+            if os.path.isfile(outputfile):
+                print("file {} already exist".format(outputfile))
+                continue
+            
+            #get data
+            try:
+                c.retrieve(
+                    dataset_name,
+                    {
+                        'product_type': 'reanalysis',
+                        'variable': variables,
+                        'year': [year],
+                        'month': [month],
+                        'day': [day],
+                        'time': times,
+                        'format': 'netcdf'
+                    },
+                    outputfile)
+            except:
+                print("Unable to retrieve information for {}-{}-{}".format(year, month, day))
+                continue
