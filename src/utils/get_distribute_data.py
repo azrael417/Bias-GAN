a7b1451515cd68fc2ea0bf59c4423a9c8614f973
Thorsten Kurth <thorstenkurth@me.com>
2020-02-14 08:40:36 -0800

    more general data grabber

--- src/utils/get_distribute_data.py
+++ src/utils/get_distribute_data.py
@@ -8,26 +8,27 @@ nraid = 4
 c = cdsapi.Client()
 
 #parameters
+root_dir = "/"
 dataset_name = 'reanalysis-era5-single-levels'
 variables = [ '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
               '2m_temperature', 'mean_sea_level_pressure', 'mean_wave_direction',
               'mean_wave_period', 'sea_surface_temperature', 'significant_height_of_combined_wind_waves_and_swell',
               'surface_pressure', 'total_precipitation']
 times = ["{:02d}:00".format(x) for x in range(0,24)]
-#days = ["{:02d}".format(x) for x in range(1,32)]
-#months = ["{:02d}".format(x) for x in range(1,13)]
+days = ["{:02d}".format(x) for x in range(1,32)]
+months = ["{:02d}".format(x) for x in range(1,13)]
 #years = ['1979', '1980', '1981', '1982']
-
-days = ["23"]
-months = ["11"]
-years = ["1981"]
+years = ['1983', '1984', '1985', '1986']
 
 #loop over years
 for idx, year in enumerate(years):
     for idy, month in enumerate(months):
 
         #distribute round robin in years 
-        path = '/data{}/ecmwf_data/gpu{}'.format(2 * (idx % nraid) + 1, (idy // 3) + idx * 4)
+        path = os.path.join(root_dir,'data{}'.format(2 * (idx % nraid) + 1), 'ecmwf_data', 'gpu{}'.format((idy // 3) + idx * 4))
+
+        print(path)
+        exit
         
         #create folder
         if not os.path.isdir(path):
