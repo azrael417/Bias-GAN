f786e430917a9a13ed481ab337daa304c183cf75
Thorsten Kurth <thorstenkurth@me.com>
2020-03-05 20:22:01 -0800

    added fuse data

---
+++ src/utils/fuse_convert_data_gpsro.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+
+        
+#global parameters
+data_format = "nchw"
+data_path_prefix = "/global/cfs/cdirs/dasrepo/tkurth/DataScience/ECMWF/data/gpsro"
+numfiles = 4
+
+files = [ x.replace("data_in_", "") for x in os.listdir(os.path.join(data_path_prefix, "raw")) if x.endswith(".npy") and x.startswith("data_in_") ]
+
+outdir = os.path.join(data_path_prefix, "all")
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+# convert to nchw
+for fname in files:
+    #data
+    data = np.load(os.path.join(data_path_prefix, "raw", "data_in_"+fname))
+    data = np.transpose(data, (2,0,1))
+    np.save(os.path.join(outdir, "data_in_"+fname), data)
+
+    #label
+    label = np.load(os.path.join(data_path_prefix, "raw", "data_out_"+fname))
+    label = np.transpose(label, (2,0,1))
+    np.save(os.path.join(outdir, "data_out_"+fname), label)
+
+    
