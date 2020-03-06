549572b1b9a2b90b09fca33297273688575a3128
Thorsten Kurth <thorstenkurth@me.com>
2020-03-05 16:03:37 -0800

    added split and summary files for the reader

--- src/utils/compute_naive_loss.py
+++ src/utils/compute_naive_loss.py
@@ -1,55 +1,121 @@
 import os
 import numpy as np
+import argparse as ap
 
-#global parameters
-nraid = 4
-overwrite = False
-data_format = "nchw"
-data_path_prefix = "/"
-channels = [0, 1, 2, 3, 4, 9]
+from mpi4py import MPI
 
-#start loop
-loss = 0.
-count = 0
-for idx in range(0,nraid):
 
-    #root path
-    root = os.path.join( data_path_prefix, 'data{}'.format(2 * idx + 1), 'ecmwf_data' )
+def main(pargs):
     
-    for gpudir in os.listdir(root):
+    #global parameters
+    nraid = pargs.num_raid
+    data_format = "nchw"
+    #data_path_prefix = "/"
+    data_dir_prefix = pargs.data_dir_prefix
+    channels = pargs.channels
+    normalization_file = pargs.normalization_file
+    normalization_type = pargs.normalization_type
 
-        if not gpudir.startswith("gpu"):
-            continue
+    #MPI stuff
+    comm = MPI.COMM_WORLD.Dup()
+    comm_rank = comm.Get_rank()
+    comm_size = comm.Get_size()
 
-        #load normalization
-        normarr = np.load(os.path.join(root, gpudir, 'train', 'stats.npz'))
-        minarr = normarr["minval"].reshape(1, normarr["minval"].shape[0], 1, 1)
-        minarr = minarr[:,channels,...]
-        maxarr = normarr["maxval"].reshape(1, normarr["maxval"].shape[0], 1, 1)
-        maxarr = maxarr[:,channels,...]
+    #get list of files on rank 0:
+    files= []
+    if comm_rank == 0:
+        
+        #raid drives
+        for idx in range(0,nraid):
 
-        #go through files in valid, normalize and compute loss
-        files = [ os.path.join(root, gpudir, 'validation', x)  for x in os.listdir(os.path.join(root, gpudir, 'validation')) \
-                  if x.endswith('.npy') and x.startswith('data-') ]
+            #root path
+            root = os.path.join( data_dir_prefix, 'data{}'.format(2 * idx + 1), 'ecmwf_data' )
 
-        for filename in files:
-            #load data and project
-            token = np.load(filename)
-            token = token[:, channels, ...]
-            #increase counter
-            current_count = token.shape[0]
-            count += current_count
-            #normalize
-            token = (token - minarr) / (maxarr - minarr)
-            #split in data and label
-            data = token[0::2]
-            label = token[1::2]
-            #compute diff
-            absdiff = np.abs(data-label)
-            current_loss = np.sum(np.mean(absdiff, axis=(1,2,3)))
-            loss += current_loss
-            #print temp loss
-            print("Average loss for file {}: {}".format(filename, current_loss / float(current_count)))
-            
-#print result
-print("Final average loss: {}", loss / float(count))
+            for gpudir in os.listdir(root):
+
+                #exclude some crappy dirs
+                if not gpudir.startswith("gpu"):
+                    continue
+
+                files += [ os.path.join(root, gpudir, 'validation', x)  for x in os.listdir(os.path.join(root, gpudir, 'validation')) \
+                           if x.endswith('.npy') and x.startswith('data-') ]
+
+        #sort the files
+        files.sort()
+        
+    #broadcast list of files to all ranks
+    files = comm.bcast(files, root = 0)
+
+    #compute bounds
+    chunksize = int(np.ceil(len(files) / comm_size))
+    start_idx = chunksize * comm_rank
+    end_idx = min([start_idx + chunksize, len(files)])
+    files = files[start_idx:end_idx]
+
+    #load normalization on rank0
+    if comm_rank == 0:
+        statsfile = np.load(normalization_file)
+        if normalization_type == "MinMax":
+            #get min and max:
+            data_shift = statsfile["minval"][channels]
+            data_scale = 1. / ( statsfile["maxval"][channels] - data_shift )
+        elif normalization_type == "MeanVariance":
+            #get <x> and <x**2>:
+            data_shift = statsfile["mean"][channels]
+            data_scale = 1. / np.sqrt( statsfile["sqmean"][channels] - np.square(data_shift) )
+    else:
+        data_shift = np.zeros(len(channels))
+        data_scale = np.zeros(len(channels))
+
+    #broadcast
+    data_shift = comm.bcast(data_shift, root = 0)
+    data_scale = comm.bcast(data_scale, root = 0)
+    
+    #reshape
+    data_shift = data_shift.reshape(1, data_shift.shape[0], 1, 1)
+    data_scale = data_scale.reshape(1, data_scale.shape[0], 1, 1)
+    
+    #start loop
+    loss = 0.
+    count = 0
+    for filename in files:
+
+        #load data and project
+        token = np.load(filename)
+        token = token[:, channels, ...]
+        #increase counter
+        current_count = token.shape[0]
+        count += current_count
+        #normalize
+        token = data_scale * (token - data_shift)
+        #split in data and label
+        data = token[0::2]
+        label = token[1::2]
+        #compute diff
+        absdiff = np.abs(data-label)
+        current_loss = np.sum(np.mean(absdiff, axis=(1,2,3)))
+        loss += current_loss
+
+    print(comm_rank, count, loss)
+
+    #reduce the loss and count
+    loss = comm.reduce(loss, root = 0)
+    count = comm.reduce(count, root = 0)
+    
+    #print result
+    if comm_rank == 0:
+        print("Final average loss: {} ({} samples)".format(loss / float(count), count))
+
+    
+if __name__ == '__main__':
+
+    #arguments
+    AP = ap.ArgumentParser()
+    AP.add_argument("--num_raid", type=int, default=4, choices=[4, 8], help="Number of available raid drives")
+    AP.add_argument("--data_dir_prefix", type=str, default='/', help="prefix to data dir")
+    AP.add_argument("--normalization_file", type=str, default='/', help="NPZ file which contains normalization data")
+    AP.add_argument("--channels", type=int, nargs='+', default=[0,1,2,3,4,5,6,7,8,9,10], help="Channels used in input")
+    AP.add_argument("--normalization_type", type=str, default="MinMax", choices=["MinMax", "MeanVariance"], help="Normalization type")
+    pargs = AP.parse_args()
+    
+    main(pargs)
---
+++ src/utils/summarize_data_gpsro.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+
+#merge function helper
+def merge_token(token1, token2):
+    #extract data
+    #first
+    n1 = token1[0]
+    dmean1 = token1[1]
+    dsqmean1 = token1[2]
+    dmin1 = token1[3]
+    dmax1 = token1[4]
+    #second
+    n2 = token2[0]
+    dmean2 = token2[1]
+    dsqmean2 = token2[2]
+    dmin2 = token2[3]
+    dmax2 = token2[4]
+
+    #create new token
+    nres = n1 + n2
+    dmeanres = (n1/nres)*dmean1 + (n2/nres)*dmean2
+    dsqmeanres = (n1/nres)*dsqmean1 + (n2/nres)*dsqmean2
+    dminres = np.minimum(dmin1, dmin2)
+    dmaxres = np.maximum(dmax1, dmax2)
+
+    return (nres, dmeanres, dsqmeanres, dminres, dmaxres)
+
+
+#create data token
+def create_token(filename, data_format="nchw"):
+    arr = np.load(filename).astype(np.float64)
+    axis = (0,2,3) if data_format == "nchw" else (0,1,2)
+
+    #how many samples do we have
+    n = arr.shape[0]
+    #compute stats
+    mean = np.mean(arr, axis=axis)
+    meansq = np.mean(np.square(arr), axis=axis)
+    minimum = np.amin(arr, axis=axis)
+    maximum = np.amax(arr, axis=axis)
+
+    #result
+    result = (n, mean, meansq, minimum, maximum)
+    return result
+        
+
+#global parameters
+nraid = 4
+overwrite = False
+data_format = "nchw"
+data_path_prefix = "/"
+
+token = None
+for idx in range(0,nraid):
+
+    #root path
+    root = os.path.join( data_path_prefix, 'data{}'.format(2 * idx + 1), 'ecmwf_data' )
+    
+    for gpudir in os.listdir(root):
+
+        if not gpudir.startswith("gpu"):
+            continue
+        
+        files = [ os.path.join(root, gpudir, 'train', x)  for x in os.listdir(os.path.join(root, gpudir, 'train')) \
+                  if x.endswith('.npy') and x.startswith('data-') ]
+
+        #get first token and then merge recursively
+        token = create_token(files[0], data_format)
+        for filename in files[1:]:
+            token = merge_token(create_token(filename, data_format), token)
+
+
+#distribute the file
+for idx in range(0,nraid):
+
+    #root path
+    root = os.path.join( data_path_prefix, 'data{}'.format(2 * idx + 1), 'ecmwf_data' )
+
+    for gpudir in os.listdir(root):
+
+        if not gpudir.startswith("gpu"):
+            continue
+        
+        #save results
+        np.savez(os.path.join(root, gpudir, "train", "stats.npz"), count=token[0], mean=token[1], sqmean=token[2], minval=token[3], maxval=token[4])
