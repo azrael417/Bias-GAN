ac3ccd03e353a6f5806378c317f2ccc7d0836e1b
Awesom E. Dev <devnull@nvidia.com>
2020-10-16 17:03:57 -0700

    fixed the general mtric averaging function

--- src/deepCam/run_scripts/run_training_regression_coccobello.sh
+++ src/deepCam/run_scripts/run_training_regression_coccobello.sh
@@ -21,7 +21,8 @@ config_name="good/regression3d_normal_comfy-sweep-4.yaml"
 mpioptions="--allow-run-as-root --map-by ppr:8:socket:PE=3"
 
 #3D manual runs
-mpirun -np ${totalranks} ${mpioptions} python ../gpsro_train/train_regression3d.py \
+mpirun -np ${totalranks} ${mpioptions} \
+       python ../gpsro_train/train_regression3d.py \
        --run_tag ${run_tag} \
        --tag_run \
        --config_file "../gpsro_configs/${config_name}"
