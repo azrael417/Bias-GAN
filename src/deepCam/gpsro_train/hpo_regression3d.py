eb4fa3f3c057bb365eeab345aba3408bc7811e89
Thorsten Kurth <tkurth@nvidia.com>
2020-12-01 07:43:37 -0800

    adding better hpo for regression and point autoencoder

--- src/deepCam/gpsro_train/hpo_regression3d.py
+++ src/deepCam/gpsro_train/hpo_regression3d.py
@@ -13,6 +13,17 @@ from utils import yparams as yp
 # the training module
 from regression3d_module import Regression3d
 
+# hpo stuff
+import ray
+from ray import tune
+from ray.tune import Trainable, run, sample_from
+from ray.tune.suggest import ConcurrencyLimiter
+from ray.tune.schedulers import AsyncHyperBandScheduler
+from hyperopt import hp
+from ray.tune.suggest.hyperopt import HyperOptSearch
+from ray.tune.integration.wandb import wandb_mixin
+from ray.tune.integration.wandb import WandbLogger
+
 hyperparameter_defaults = dict(
     max_intra_threads = 1,
     num_workers = 1,
@@ -29,6 +40,19 @@ hyperparameter_defaults = dict(
     channels = list(range(0,45)),
 )
 
+
+def train_wrapper(config, checkpoint_dir=None):
+
+    # initialize model
+    r3d = Regression3d(config)
+
+    # train
+    r3d.train()
+
+    # return min loss:
+    tune.report(validation_loss = min(r3d.validation_losses))
+
+    
 # main function
 def main(pargs):
     
@@ -43,12 +67,58 @@ def main(pargs):
     # add run tag if requested
     if pargs.run_tag is not None:
         config["run_tag"] = pargs.run_tag
-        
-    # initialize model
-    r3d = Regression3d(config)
+
+    if pargs.group_tag is not None:
+        config["group_tag"] = pargs.group_tag
+
+    # get wandb api token
+    with open("/certs/.wandbirc_gpsro") as f:
+        wbtoken = f.readlines()[0].replace("\n","")
+
+    # add wandb info
+    config["wandb"] = {"project": "GPSRO bias correction",
+                       "api_key": wbtoken,
+                       "monitor_gym": False}
     
-    # train
-    r3d.train()    
+    # init ray
+    ray.init()
+
+    # override config
+    tune_config = {'local_batch_size':  hp.choice('local_batch_size', config['local_batch_size']),
+                   'start_lr': hp.loguniform('start_lr', np.log(config['start_lr']['min']), np.log(config['start_lr']['max'])),
+                   'weight_decay': hp.loguniform('weight_decay', np.log(config['weight_decay']['min']), np.log(config['weight_decay']['max'])),
+                   'layer_normalization': hp.choice('layer_normalization', ["instance_norm", "batch_norm"]),
+                   'lr_schedule': hp.choice('lr_schedule', config['lr_schedule'])
+    }
+
+    # update config:
+    for key in tune_config.keys():
+        config[key] = tune_config[key]
+
+    tune_kwargs = {'num_samples': config["num_trials"],
+                   'config': config}
+
+    current_best_params = [{"local_batch_size": 1,
+                            "start_lr": 0.00303,
+                            'lr_schedule': 2,
+                            "weight_decay": 0.01,
+                            'layer_normalization': 1}]
+
+    # create scheduler and search
+    #scheduler = AsyncHyperBandScheduler()
+    algo = HyperOptSearch(config, points_to_evaluate = current_best_params, metric='validation_loss', mode='min')
+    algo = ConcurrencyLimiter(algo, max_concurrent=config["max_concurrent_trials"])
+
+    # run the training
+    tune.run(train_wrapper,
+             #loggers=[WandbLogger],
+             resources_per_trial={'gpu': 1},
+             num_samples=config["num_trials"],
+             search_alg=algo)
+
+    # goodbye
+    ray.shutdown()
+    
 
 if __name__ == "__main__":
 
@@ -57,6 +127,7 @@ if __name__ == "__main__":
     AP.add_argument("--checkpoint", type=str, default=None, help="Checkpoint file to restart training from.")
     AP.add_argument("--config_file", type=str, default=None, help="YAML file to read config data from. If none specified, use WandB")
     AP.add_argument("--run_tag", type=str, default=None, help="A tag to identify the run")
+    AP.add_argument("--group_tag", type=str, default=None, help="A tag to group runs")
     pargs, _ = AP.parse_known_args()
     
     #run the stuff
--- src/utils/gpsro_point_preprocess.py
+++ src/utils/gpsro_point_preprocess.py
@@ -1,5 +1,6 @@
 import os
 import re
+import itertools
 import numpy as np
 import pandas as pd
 from scipy.interpolate import SmoothBivariateSpline as interp
@@ -26,6 +27,7 @@ if not os.path.isdir(output_dir):
 # open level file and select
 metadf = pd.read_csv(level_file)
 metadf = metadf[ metadf["level"].isin(levels) ].sort_values(by="level", ascending = True).reset_index(drop = True)
+altitudes = [ (earth_radius + x) / earth_radius for x in metadf[ altitude_tag ].values.tolist() ]
     
 # iterate over tags
 for tag in tqdm(tags):
@@ -38,11 +40,26 @@ for tag in tqdm(tags):
     # load data out
     data_out = np.load(os.path.join(file_root,"data_out_raw_"+tag+".data"), allow_pickle=True, encoding='latin1')
 
-    # 
-    print(lon, data_in)
-    sys.exit(1)
+    # compute spherical coordinates
+    phi = [ np.pi * (x + 180.) / 180. for x in lon ]
+    theta = [ np.pi * (90. - x) / 180. for x in lat ]
+
+    # compute x, y, z:
+    xcoords = [ a * np.cos(p) * np.sin(t) for a, p, t in zip(altitudes, phi, theta) ]
+    ycoords = [ a * np.sin(p) * np.sin(t) for a, p, t in zip(altitudes, phi, theta) ]
+    zcoords = [ a * np.cos(t) for a, p, t in zip(altitudes, phi, theta) ]
+
+    # flatten
+    xcoords = np.array(list(itertools.chain(*[ x.tolist() for x in xcoords ])), dtype=np.float32)
+    ycoords = np.array(list(itertools.chain(*[ y.tolist() for y in ycoords ])), dtype=np.float32)
+    zcoords = np.array(list(itertools.chain(*[ z.tolist() for z in zcoords ])), dtype=np.float32)
+    data_in = np.array(list(itertools.chain(*[ d.tolist() for d in data_in ])), dtype=np.float32)
+    data_out = np.array(list(itertools.chain(*[ d.tolist() for d in data_out ])), dtype=np.float32)
+
+    # compute final arrays
+    data_in = np.stack([xcoords, ycoords, zcoords, data_in], axis = 1)
+    data_out = np.stack([xcoords, ycoords, zcoords, data_out], axis = 1)
     
     # store results
-    np.save(os.path.join(output_dir, "data_in_" + tag + ".npy"), data_in_arr)
-    np.save(os.path.join(output_dir, "data_out_" + tag + ".npy"), data_out_arr)
-    np.save(os.path.join(output_dir, "masks_" + tag + ".npy"), mask_arr)
+    np.save(os.path.join(output_dir, "data_in_" + tag + ".npy"), data_in)
+    np.save(os.path.join(output_dir, "data_out_" + tag + ".npy"), data_out)
