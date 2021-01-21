a740f7ff1822c1e49fbbd842bc966216a70fdde2
Awesom E. Dev <devnull@nvidia.com>
2021-01-20 23:09:37 -0800

    more spherical convs

--- src/deepCam/architecture/common/spherical.py
+++ src/deepCam/architecture/common/spherical.py
@@ -34,7 +34,7 @@ def SphYCoeff(l, m):
 
 class SphericalFFT(nn.Module):
 
-    def __init__(self, lmax, num_in_channels, num_out_channels):
+    def __init__(self, lmax, num_in_channels, num_out_channels, theta_out=None, phi_out=None):
         super(SphericalFFT, self).__init__()
         self.lmax = lmax
         self.n_in = num_in_channels
@@ -50,6 +50,10 @@ class SphericalFFT(nn.Module):
             for m in range(0, l+1):
                 self.coeffs[(l,m)] = coeff * SphYCoeff(l, m)**2
                 self.elem_count	+= 1
+        
+        # output arrays
+        self.theta_out = theta_out
+        self.phi_out = phi_out
 
         # init weights: only m=0 are relevant
         self.weights = nn.ParameterList([torch.randn((self.n_in, self.n_out)) for i in range(self.lmax)])
@@ -68,31 +72,49 @@ class SphericalFFT(nn.Module):
         
         # prereqs
         # azimuth
-        exp_mimphi = {}
+        exp_mimphi_in = {}
         for m in range(1, self.lmax+1):
             cos_mphi = torch.cos(m*phi)
             sin_mphi = torch.sin(m*phi)
-            exp_mimphi[m] = torch.complex(cos_mphi, -sin_mphi)
+            exp_mimphi_in[m] = torch.complex(cos_mphi, -sin_mphi)
+            
+        # if resampling is requested, do it here
+        if self.phi_out is not None:
+            exp_mimphi_out = {}
+            for m in range(1, self.lmax+1):
+                cos_mphi = torch.cos(m*self.phi_out)
+                sin_mphi = torch.sin(m*self.phi_out)
+                exp_mimphi_out[m] = torch.complex(cos_mphi, -sin_mphi)
+        else:
+            exp_mimphi_out = exp_mimphi_in
 
         # polar
         cos_theta = torch.cos(theta)
-        leg_l = {}
+        leg_l_in = {}
         for l in range(0, self.lmax+1):
-            leg_l[l] = LegendreP(l, cos_theta)
+            leg_l_in[l] = LegendreP(l, cos_theta)
+        
+        # if resampling is requested, do it here
+        if self.theta_out is not None:
+            cos_theta = torch.cos(self.theta_out)
+            leg_l_out = {}
+            for l in range(0, self.lmax+1):
+                leg_l_out[l] = LegendreP(l, cos_theta)
+        else:
+            leg_l_out = leg_l_in
         
         # init result
-        #tz = torch.zeros((theta.shape[0], num_out_channels), dtype=theta.dtype)
         results = []
 
         # l = m = 0
-        prod = areas * leg_l[0] * torch.matmul(values, self.weights[0])
+        prod = areas * leg_l_in[0] * torch.matmul(values, self.weights[0])
         results.append(self.coeffs[(0,0)] * torch.sum(prod, dims=1))
         
         # compute the SFT
         count = 1
         for l in range(1, self.lmax+1):
             # legendre polynomial
-            leg = leg_l[l]
+            leg = leg_l_in[l]
 
             # m = 0
             prod = areas * leg * torch.matmul(values, self.weights[l])
@@ -101,16 +123,16 @@ class SphericalFFT(nn.Module):
             
             # m > 0
             for m in range(1, l+1):
-                results.append(self.coeffs[(l,m)] * torch.sum(prod * exp_mimphi[m], dims=1))
+                results.append(self.coeffs[(l,m)] * torch.sum(prod * exp_mimphi_in[m], dims=1))
                 count += 1
 
         # compute the ISFT: coefficients are already taken care of
         # l = m = 0
-        result = results[0] * leg_l[0]
+        result = results[0] * leg_l_out[0]
         count = 1
         for l in range(1, self.lmax+1):
             # legendre polynomial
-            leg = leg_l[l]
+            leg = leg_l_out[l]
 
             # m = 0
             result += results[count] * leg
@@ -119,7 +141,7 @@ class SphericalFFT(nn.Module):
             # m > 0
             for m in range(1, l+1):
                 # we have a + instead of a minus because we are using Ybar_lm not Y_lm
-                tmp = torch.real(results[count]) * torch.real(exp_mimphi[m]) + torch.imag(results[count]) * torch.imag(exp_mimphi[m])
+                tmp = torch.real(results[count]) * torch.real(exp_mimphi_out[m]) + torch.imag(results[count]) * torch.imag(exp_mimphi_out[m])
                 result += leg * 2. * tmp
                     
         return result
